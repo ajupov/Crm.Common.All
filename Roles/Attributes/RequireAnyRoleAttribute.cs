370f47fb45c78658e2bf752db52fd87cffd2a0f3
Usman Ayupov <u.ayupov@dodopizza.com>
2020-02-07 23:42:57 +0300

    Add RequireAnyRoleAttribute

---
+++ Roles/Attributes/RequireAnyRoleAttribute.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Crm.Common.All.Roles.Attributes
+{
+    public class RequireAnyRoleAttribute : AuthorizeAttribute
+    {
+        public RequireAnyRoleAttribute(string oauthAuthenticationScheme)
+        {
+            AuthenticationSchemes = $"{JwtBearerDefaults.AuthenticationScheme},{oauthAuthenticationScheme}";
+        }
+    }
+}

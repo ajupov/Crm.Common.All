5e97f159fc6a5937d9499a2ab0c7dff177989460
Usman Ayupov <u.ayupov@dodopizza.com>
2021-11-21 12:10:49 +0300

    Move NonDefaultGuidAttribute

---
+++ Validation/Attributes/NonDefaultGuidAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace Crm.Common.All.Validation.Attributes
+{
+    public class NonDefaultGuidAttribute : ValidationAttribute
+    {
+        private const string DefaultErrorMessage = "'{0}' does not contain a valid guid";
+
+        public NonDefaultGuidAttribute()
+            : base(DefaultErrorMessage)
+        {
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var input = Convert.ToString(value, CultureInfo.CurrentCulture);
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return new ValidationResult(FormatErrorMessage(validationContext.DisplayName));
+            }
+
+            if (!Guid.TryParse(input, out var guid))
+            {
+                return new ValidationResult(FormatErrorMessage(validationContext.DisplayName));
+            }
+
+            return guid == Guid.Empty
+                ? new ValidationResult(FormatErrorMessage(validationContext.DisplayName))
+                : ValidationResult.Success;
+        }
+    }
+}
